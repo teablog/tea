5b3170401746dc2ebfe9f4585878fdcf3b54539e
刘宁 <liuning@qutoutiao.net>
2020-08-26 00:25:12 +0800

    github

--- internal/module/account/github.go
+++ internal/module/account/github.go
@@ -91,7 +91,7 @@ func (g *_github) User() (err error) {
 	authorization.WriteString(" ")
 	authorization.WriteString(g.t.AccessToken)
 	query := gin.H{
-		"url": "https://tea.github.com/user",
+		"url": "https://api.github.com/user",
 		"method": "GET",
 		"header": gin.H{
 			"Authorization": authorization.String(),
