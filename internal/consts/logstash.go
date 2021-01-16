a2c7f3943dbe0c430240b2e92f62f86899c84728
douyacun <douyacun@gmail.com>
2021-01-16 17:37:37 +0800

    index_template

--- internal/consts/logstash.go
+++ internal/consts/logstash.go
@@ -2,6 +2,19 @@ package consts
 
 const (
 	SpiderIndices = "spider_%s"
+	SpiderIndexTemplate = `{
+  "index_patterns": ["spider_*"],  
+  "mappings": {
+    "properties": {
+      "date": {
+        "type": "date"
+      },
+      "spider": {
+        "type": "keyword"
+      }
+    }
+  }
+}`
 )
 
 
