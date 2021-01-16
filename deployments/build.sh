5a0414cb707dc8641964713ea7ebb68cde68d50f
douyacun <douyacun@gmail.com>
2021-01-16 22:03:12 +0800

    build

--- deployments/build.sh
+++ deployments/build.sh
@@ -3,8 +3,17 @@
 # shellcheck disable=SC2164
 CURDIR=$(cd "$(dirname "$0")"; pwd)
 
+echo '--- build docker image';
 docker build -t registry.cn-hangzhou.aliyuncs.com/douyacun/tea:latest .
+
+echo '--- push image to aliyun';
 docker push registry.cn-hangzhou.aliyuncs.com/douyacun/tea:latest
+
+echo "--- deploy tea";
 ssh douyacun < "${CURDIR}"/deploy.sh
 
+echo "--- sync image...";
+sh "${CURDIR}"/sync_image.sh
+
+echo "--- docker image prune..."
 echo y|docker image prune
