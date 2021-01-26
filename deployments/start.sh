25a4e2936c35f2bd9f80224d8ae13409ef130748
liuning <douyacun@gmail.com>
2021-01-26 14:28:37 +0800

    start.sh

--- deployments/start.sh
+++ deployments/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 CURDIR=$(cd "$(dirname "$0")"; pwd)
 
-echo "> 重启ssl证书架空..."
-ps -aux|grep ssl/watch.sh|grep -v "grep"|awk '{print $2}'|xargs kill -9
+echo "> 重启ssl证书监控..."
+for var in $(ps -aux|grep ssl/watch.sh|grep -v "grep"|awk '{print $2}')
+do
+  if [ ! -n "$var" ]; then
+      kill -9 $var
+  fi
+done
 nohup $CURDIR/ssl/watch.sh >> $CURDIR/ssl/watch.log 2>&1 &
