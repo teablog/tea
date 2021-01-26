8166f36b7d28665cddb28bb5429d21ae2f5eb392
liuning <douyacun@gmail.com>
2021-01-26 14:32:59 +0800

    watch

--- deployments/start.sh
+++ deployments/start.sh
@@ -4,9 +4,7 @@ CURDIR=$(cd "$(dirname "$0")"; pwd)
 echo "> restart ssl cert watch..."
 for var in $(ps -aux|grep watch.sh|grep -v "grep"|awk '{print $2}')
 do
-  if [ ! -n "$var" ]; then
-      echo "> kill -9 ${var}"
-      kill -9 $var
-  fi
+    echo "> kill -9 ${var}"
+    kill -9 $var
 done
 nohup $CURDIR/ssl/watch.sh >> $CURDIR/ssl/watch.log 2>&1 &
