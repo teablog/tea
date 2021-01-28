1f65f54816fa2d65f762ae9009bb11927029e86c
liuning <douyacun@gmail.com>
2021-01-28 16:34:18 +0800

    kafka

--- deployments/kafka/start.sh
+++ deployments/kafka/start.sh
@@ -3,4 +3,14 @@ KAFKADIR=/data/kafka
 KAFKABIN=$KAFKADIR/kafka_2.11-0.11.0.3/bin/
 KAFKACONF=$KAFKADIR/kafka_2.11-0.11.0.3/config
 
-sh $KAFKABIN/zookeeper-server-start.sh --daemon $KAFKACONF/zookeeper.properties
+echo "> start zookeeper"
+sh $KAFKABIN/zookeeper-server-start.sh --daemon $KAFKACONF/zookeeper.properties;
+
+n = 3
+for (( i = 0; i < n; i++ )); do
+    echo "."
+    sleep 1;
+done
+
+echo "> start kafka";
+sh $KAFKABIN/kafka-server-start.sh --daemon $KAFKACONF/server.properties;
